206ead72cf8603580bc891fe45d8b9de6e7f7c56
thiagobcf <thiagobcfonseca@gmail.com>
2023-09-04 19:09:51 -0300

    cadastrando na admin django

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericStackedInline
+
+from tag.models import Tag
 
 from .models import Category, Recipe
 
 
 class CategoryAdmin(admin.ModelAdmin):
     ...
+
+
+class TagInLine(GenericStackedInline):
+    model = Tag
+    fields = 'name',
+    extra = 1
 
 
 @admin.register(Recipe)
@@ -20,6 +29,10 @@ class RecipeAdmin(admin.ModelAdmin):
     prepopulated_fields = {
         "slug": ('title',)
     }
+
+    inlines = [
+        TagInLine,
+    ]
 
 
 admin.site.register(Category, CategoryAdmin)
