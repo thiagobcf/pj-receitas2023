68fa00976370418c1fa7def6595d55a6343b1d99
thiagobcf <thiagobcfonseca@gmail.com>
2023-09-05 11:13:40 -0300

    ManyToManyField para tags

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,19 +1,10 @@
 from django.contrib import admin
-from django.contrib.contenttypes.admin import GenericStackedInline
-
-from tag.models import Tag
 
 from .models import Category, Recipe
 
 
 class CategoryAdmin(admin.ModelAdmin):
     ...
-
-
-class TagInLine(GenericStackedInline):
-    model = Tag
-    fields = 'name',
-    extra = 1
 
 
 @admin.register(Recipe)
@@ -29,10 +20,7 @@ class RecipeAdmin(admin.ModelAdmin):
     prepopulated_fields = {
         "slug": ('title',)
     }
-
-    inlines = [
-        TagInLine,
-    ]
+    autocomplete_fields = 'tags',
 
 
 admin.site.register(Category, CategoryAdmin)
